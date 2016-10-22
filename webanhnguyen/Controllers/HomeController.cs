c0b68705606a101cefd5b68a8365c34c30a60695
khoi tran <khoitran@misfit.com>
2016-10-22 21:16:39 +0700

    Bỏ code lên lần đầu tiên

---
+++ webanhnguyen/Controllers/HomeController.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webanhnguyen.Models;
+
+namespace webanhnguyen.Controllers
+{
+    public class HomeController : Controller
+    {
+        databaseDataContext db = new databaseDataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #region sản phẩm hot
+        [ChildActionOnly]
+        public ActionResult Hot_Product()
+        {
+            var SP_hot = (from sp in db.tbl_Products
+                                   where sp.Status == true
+                                   orderby sp.SLDaBan descending
+                                   select sp).Skip(0).Take(8).ToList();
+            return PartialView(SP_hot);
+        }
+        #endregion
+        #region sản phẩm tươi sống
+        [ChildActionOnly]
+        public ActionResult HaiSanTuoiSong()
+        {
+           var TuoiSong = (from sp in db.tbl_Products
+                                where sp.Status == true && sp.IDLoaiSP == 1
+                                select sp).Skip(0).Take(12).ToList();
+            return PartialView(TuoiSong);
+        }
+        #endregion
+        #region sản phẩm đông lạnh
+        [ChildActionOnly]
+        public ActionResult HaiSanDongLanh()
+        {
+            var dong = (from sp in db.tbl_Products
+                        where sp.Status == true && sp.IDLoaiSP == 2
+                        select sp).Skip(0).Take(12).ToList();
+            return PartialView(dong);
+        }
+        #endregion
+        #region sản phẩm khô
+        [ChildActionOnly]
+        public ActionResult HaiSanKho()
+        {
+             var Kho = (from sp in db.tbl_Products
+                           where sp.Status == true && sp.IDLoaiSP == 3
+                           select sp).Skip(0).Take(12).ToList();
+            return PartialView(Kho);
+        }
+        #endregion
+        #region món ăn mỗi ngày
+        [ChildActionOnly]
+        public ActionResult monanmoingay()
+        {   ViewBag.tinmoinhat = (from sp in db.tbl_news
+                                  where sp.status == true
+                                  orderby sp.NgayCapNhat descending
+                                  select sp).Skip(0).Take(1).ToList();
+            var tt = (from sp in db.tbl_news
+                       where sp.status == true 
+                       orderby sp.NgayCapNhat descending
+                       select sp).Skip(1).Take(4).ToList();
+            return PartialView(tt);
+        }
+        #endregion
+        #region chi tiết sản phẩm
+        public ActionResult Details(int id)
+        {
+            var CT_SP = (db.tbl_Products.First(sp => sp.ID == id));
+            ViewBag.SP_cungloai = (from s in db.tbl_Products
+                                   where s.ID != id && s.Status == true 
+                                   select s).Skip(0).Take(4).ToList();
+            ViewBag.TenLoai = (from s in db.tbl_Products
+                from l in db.tbl_product_types
+                where s.ID == id && s.IDLoaiSP == l.ID && s.Status == true
+                select l);
+            ViewBag.SP_khac = (from s in db.tbl_Products
+                                   where s.ID != id && s.Status == true
+                                   select s).Skip(0).Take(4).ToList();
+            CT_SP.LuotXem += 1;
+            UpdateModel(CT_SP);
+            db.SubmitChanges();
+            return View(CT_SP);
+        }
+        #endregion
+        #region sản phẩm theo loại
+        public ActionResult ProductType(int id, string sortOrder)
+        {
+
+            var laysp = from g in db.tbl_Products
+                         from h in db.tbl_product_types 
+                        where g.ID == id && g.Status == true && h.Status == true
+                         select g;
+            
+            Session["loai"] = id;
+            ViewBag.NameSortAsc = String.IsNullOrEmpty(sortOrder) ? "name_asc" : "";
+            ViewBag.NameSortDesc = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    laysp = laysp.OrderByDescending(s => s.TenSP);
+                    break;
+                case "name_asc":
+                    laysp = laysp.OrderBy(s => s.TenSP);
+                    break;
+               
+            }
+            ViewBag.TenLoai = (from l in db.tbl_product_types
+                               where l.ID == id
+                               select l);
+                        return View(laysp.ToList());
+        }
+
+        #endregion
+        public ActionResult hienthi2(int id, string sortOrder)
+        {
+          
+            var laysp = from g in db.tbl_Products
+                         from h in db.tbl_product_types
+                         where g.ID == id && g.Status == true && h.Status == true
+                                                  select g;
+            ViewBag.TenLoai = (from l in db.tbl_product_types
+                               where l.ID == id
+                               select l);
+            Session["loai"] = id;
+            return View(laysp.ToList());       
+    }
+        public ActionResult hienthi3(int id, string sortOrder)
+        {
+            var laysp = from g in db.tbl_Products
+                         from h in db.tbl_product_types
+                         where g.ID == id && g.Status == true && h.Status == true
+                         select g;
+            ViewBag.TenLoai = (from l in db.tbl_product_types
+                               where l.ID == id
+                               select l);
+            Session["loai"] = id;
+            return View(laysp.ToList());
+        }
+
+        #region sản phẩm theo loại
+        public ActionResult Laycatuoi()
+        {
+            var laysp = (from g in db.tbl_Products
+                         orderby g.NgayCapNhat descending
+                         select g).ToList();
+            ViewBag.tenmenu = (from s in db.tbl_menus where s.id == 5  select s.TenMenu);
+            tbl_menu tenmenu = db.tbl_menus.SingleOrDefault(n => n.id == 5);
+            Session["TenMenu"] = tenmenu.TenMenu;
+            return View(laysp);
+        }
+        
+        public ActionResult hienthica2()
+        {
+            var laysp = (from g in db.tbl_Products
+                         orderby g.NgayCapNhat descending
+                         select g).ToList();
+            ViewBag.tenmenu = (from s in db.tbl_menus where s.id == 5 select s);
+            return View(laysp);
+        }
+        public ActionResult hienthica3()
+        {
+            var laysp = (from g in db.tbl_Products
+                         orderby g.NgayCapNhat descending
+                         select g).ToList();
+            ViewBag.tenmenu = (from s in db.tbl_menus where s.id == 5 select s);
+            return View(laysp);
+        }
+        #endregion
+        #region Sản phẩm tìm kiếm (Search)
+        public ActionResult Search(string searchString)
+        {
+            //Lấy ra danh sách sản phẩm từ chuỗi tìm kiếm truyền vào
+            var SP_TK = (from sp in db.tbl_Products
+                         where (sp.Status == true) && (sp.TenSP.Contains(searchString)) 
+                         orderby sp.GiaHienTai descending                                         
+                         select sp).ToList();
+
+           
+            ViewBag.TuKhoa = searchString;
+
+            return View(SP_TK);
+        }
+        #endregion
+        #region menutop
+        [ChildActionOnly]//Gọi từ View sang Controll
+        public ActionResult MenuTop()
+        {
+            //Lấy ra danh sách Menu
+            var MenuTop = (from mn in db.tbl_menus
+                              orderby mn.ThuTu
+                              select mn).ToList();
+            return PartialView(MenuTop);
+        }
+        #endregion
+        #region header
+        [ChildActionOnly]//Gọi từ View sang Controll
+        public ActionResult header()
+        {
+            //Lấy ra danh sách Menu
+            var header = (from mn in db.tbl_headers
+                           select mn).ToList();
+            return PartialView(header);
+        }
+        #endregion
+        #region tintuc
+        public ActionResult tintuc()
+        {
+            var tintuc = (from tt in db.tbl_news
+                     where tt.status == true
+                     orderby tt.NgayCapNhat descending
+                     select tt);
+            return View(tintuc);
+        }
+        #endregion
+        #region Chi tiết tin (Reader)
+        public ActionResult Reader(int id)
+        {
+            //Lấy ra tin tức từ mã tin truyền vào
+            var CT_Tin = (db.tbl_news.First(tt => tt.id == id));
+
+            //Lấy ra 10 tin khác (10 tin trong đó không có tin đang đọc)
+           
+            //Bộ đếm lượt xem cho Tin tức
+            CT_Tin.LuotXem += 1;
+            UpdateModel(CT_Tin);
+            db.SubmitChanges();
+
+            return View(CT_Tin);
+        }
+
+        #endregion
+        #region khuyenmai
+        public ActionResult khuyenmai()
+        {
+            var km = (from k in db.tbl_Products
+                          where k.Status == true && k.KhuyenMai > 0
+                        
+                          orderby k.NgayCapNhat descending
+                         
+                          select k).ToList();
+            return View(km);
+        }
+        public ActionResult khuyenmai1()
+        {
+            var km = (from k in db.tbl_Products
+                      where k.Status == true && k.KhuyenMai > 0
+
+                      orderby k.NgayCapNhat descending
+
+                      select k).ToList();
+            return View(km);
+        }
+        public ActionResult khuyenmai2()
+        {
+            var km = (from k in db.tbl_Products
+                      where k.Status == true && k.KhuyenMai > 0
+
+                      orderby k.NgayCapNhat descending
+
+                      select k).ToList();
+            return View(km);
+        }
+        #endregion
+    }
+}
---
+++ webanhnguyen/Controllers/UserController.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webanhnguyen.Models;
+using System.IO;
+namespace webanhnguyen.Controllers
+{
+    public class UserController : Controller
+    {
+        databaseDataContext db = new databaseDataContext();
+        // GET: User
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #region Tài khoản (Login - SuccessLogin - Logout - Account - ChangePassword - Register - SuccessRegister)
+        #region Đăng nhập (Login)
+        [HttpGet]
+    
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+       
+        public ActionResult Login(FormCollection collection)
+        {
+                try
+                {
+                    //Lấy giá trị ở Form Login
+                    var _Email = collection["email"];
+                    var _Password = collection["password"];
+                Customer kh = db.Customers.SingleOrDefault(n => n.email == _Email && n.password == _Password);
+                //Tạo biến _UserLogin để kiểm tra tài khoản đăng nhập có trong CSDL không
+               // var _UserLogin = db.Customers.SingleOrDefault(k => k.email == _Email && k.password == _Password);
+                    if (ModelState.IsValid && kh != null)
+                    {
+                    if (kh.status == true)//không bị lock tài khoản
+                    {
+                        Session["ID"] = kh.id;
+                        //Lưu các thông tin vào Session
+                        Session["Email"] = kh;
+                        Session["email"] = kh.email;
+                        if (Session["Giohang"] == null)
+                        {//Chuyển đến trang thông báo Login thành công (Ở đây không dùng được RedirectToAction vì [ChildActionOnly])
+                            return Content("<script>alert('Đăng nhập thành công!');window.location='/Home/Index';</script>");
+                        }
+                        else
+                            return Content("<script>alert('Đăng nhập thành công!');window.location='/ShoppingCart/GioHang';</script>");
+                    }
+                    
+                        else
+                            return Content("<script>alert('Tài khoản của bạn đã bị khóa. Vui lòng liên hệ ban quản trị!');window.location='/User';</script>");
+                    }
+                    else
+                        return Content("<script>alert('Tên đăng nhập hoặc mật khẩu không đúng!');window.location='/User';</script>");
+                }
+                catch
+                {
+                    return Content("<script>alert('Đăng nhập thất bại!');window.location='/User';</script>");
+                }
+        }
+
+        #endregion
+        [HttpGet]
+        [ChildActionOnly]
+        public ActionResult plogin()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        [ChildActionOnly]
+        public ActionResult plogin(FormCollection collection)
+        {
+            
+                try
+                {
+                //Lấy giá trị ở Form Login
+
+                var _Email = collection["email"];
+                var _Password = collection["password"];
+                Customer kh = db.Customers.SingleOrDefault(n => n.email == _Email && n.password == _Password);
+                //Tạo biến _UserLogin để kiểm tra tài khoản đăng nhập có trong CSDL không
+                // var _UserLogin = db.Customers.SingleOrDefault(k => k.email == _Email && k.password == _Password);
+                if (ModelState.IsValid && kh != null)
+                {
+                    if (kh.status == true)//không bị lock tài khoản
+                    {
+                        Session["ID"] = kh.id;
+                        //Lưu các thông tin vào Session
+                        Session["Email"] = kh;
+                        Session["email"] = kh.email;
+                        if (Session["Giohang"] == null)
+                            {//Chuyển đến trang thông báo Login thành công (Ở đây không dùng được RedirectToAction vì [ChildActionOnly])
+                            return Content("<script>window.location='/Home/Index';</script>");
+                        }
+                            else
+                            return Content("<script>window.location='/ShoppingCart/GioHang';</script>");
+                    }
+                        else
+                            return Content("<script>alert('Tài khoản của bạn đã bị khóa. Vui lòng liên hệ ban quản trị!');window.location='/User';</script>");
+                    }
+                    else
+                        return Content("<script>alert('Tên đăng nhập hoặc mật khẩu không đúng!');window.location='/User';</script>");
+                }
+                catch
+                {
+                    return Content("<script>alert('Đăng nhập thất bại!');window.location='/User';</script>");
+                }
+            
+                return PartialView();
+        }
+
+     
+
+        #region Đăng xuất (Logout)
+        public ActionResult Logout()
+        {
+            Session.RemoveAll();
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
+
+        
+        #region Thay đổi thông tin cá nhân (ProfileUpdate)
+        public ActionResult ProfileUpdate()
+        {
+            if (Session["Email"] == null)
+                return RedirectToAction("Index");
+            int _MaKH = int.Parse(Session["ID"].ToString());
+            var kh = db.Customers.SingleOrDefault(k => k.id == _MaKH);
+            return View(kh);
+        }
+
+        [HttpPost]
+        public ActionResult ProfileUpdate(FormCollection collection)
+        {
+            
+            try
+            {
+                  
+                //Lấy giá trị ở Form ChangePassword
+                string _Email = collection["email"];
+                string _PassNew = collection["password1"];
+                string _RePassNew = collection["password2"];
+                string _Name = collection["name"];
+                string _SDT = collection["phone"];
+                string _Address = collection["address"];
+                string _State = collection["state"];
+                string _BillingName = collection["billingname"];
+                string _BillingPhone = collection["billingphone"];
+                string _BillingAddress = collection["billingaddress"];
+                string _BillingState = collection["billingstate"];
+                int _MaKH = int.Parse(Session["ID"].ToString());
+                var kh = db.Customers.SingleOrDefault(k => k.id == _MaKH);
+                if (collection["ship_to_another"] == "1")
+                {
+                    kh.name = _Name;
+                    kh.moblie = _SDT;
+                    kh.address = _Address;
+                    kh.state = _State;
+                    kh.password = _PassNew;
+                    kh.billingname = _BillingName;
+                    kh.billingphone = _BillingPhone;
+                    kh.billingaddress = _BillingAddress;
+                    kh.billingstate = _BillingState;
+                }
+                else
+                {
+                    kh.name = _Name;
+                    kh.moblie = _SDT;
+                    kh.address = _Address;
+                    kh.state = _State;
+                    kh.password = _PassNew;
+                    kh.billingname = _Name;
+                    kh.billingphone = _SDT;
+                    kh.billingaddress = _Address;
+                    kh.billingstate = _State;
+                }
+                UpdateModel(kh);
+                db.SubmitChanges();
+                return Content("<script>alert('Cập nhật thành công!');window.location='/User/ProfileUpdate';</script>");
+            }
+            catch
+            {
+                return Content("<script>alert('Thao tác đổi mật khẩu thất bại!');window.location='/User/ProfileUpdate';</script>");
+            }
+        }
+        #endregion
+
+        #region Đăng ký (Register)
+        public ActionResult Register()
+        {
+            //Đã Đăng Nhập thì không cho vào Form Đăng Ký
+            if (Session["Email"] != null)
+                return RedirectToAction("Index");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(FormCollection collection, Customer kh)
+        {
+
+            try
+            {
+                //Lấy giá trị ở Form Register         
+                string _Email = collection["email"];
+                string _Password = collection["password1"];
+                string _RePassword = collection["password2"];
+                //Kiểm tra xem tài khoản đã có người sử dụng chưa?
+                var _CheckUser = db.Customers.FirstOrDefault(k => k.email == _Email);
+                if (_CheckUser != null)
+                    return Content("<script>alert('Email đã có người sử dụng!');window.location='/User/Register';</script>");
+                else
+                    kh.email = _Email;
+
+                //Kiểm tra Mật khẩu nhập lại có giống Mật khẩu đăng ký không?
+                    kh.password = _Password;
+                kh.date_added = DateTime.Now;
+                //Khai báo _FileUpload ở <input type="file" id="_FileUpload" name="_FileUpload" /> trên Form Register
+                kh.status = true;//Mặc định cho tài khoản là Hiện
+
+                //Thực hiện thêm mới
+                db.Customers.InsertOnSubmit(kh);
+                db.SubmitChanges();
+
+                //Lưu thông tin khách hàng vừa đăng ký vào Session để tự động đăng nhập
+                Session.Add("ID", kh.id);
+                Session.Add("Email", _Email);
+                return RedirectToAction("SuccessRegister");
+            }
+            catch
+            {
+                return Content("<script>alert('Đăng ký thất bại!');window.location='/User/Register';</script>");
+            }
+        }
+        #endregion
+
+        #region Đăng ký thành công (SuccessRegister)
+        public ActionResult SuccessRegister()
+        {
+            //Ở đây khi đăng ký thành công thì sẽ lưu thông tin tài khoản vào Session => chưa có thì về trang chủ
+            if (Session["Email"] == null)
+                return RedirectToAction("Index");
+
+            return View();
+        }
+        #endregion
+        #region quên mật khẩu (RecoverPassword)
+        public ActionResult RecoverPassword()
+        {
+            //Ở đây khi đăng ký thành công thì sẽ lưu thông tin tài khoản vào Session => chưa có thì về trang chủ
+            if (Session["Email"] == null)
+                return RedirectToAction("Index");
+            return View();
+        }
+        #endregion
+        #region Đơn hàng
+        public ActionResult Order()
+        {
+            if (Session["Email"] == null)
+                return RedirectToAction("Index");
+            int _MaKH = int.Parse(Session["ID"].ToString());
+            var donhang = (from d in db.Orders
+                           where d.idkh == _MaKH
+                           select d).ToList();
+            return View(donhang);
+        }
+        #endregion
+        #region Đổi trả hàng
+        public ActionResult Returns()
+        {
+            return View();
+        }
+        #endregion
+        #region chi tiết đơn hàng
+        public ActionResult OrderDetail(int id)
+        {
+
+            var CT_SP = from c in db.OrderDetails
+                        from d in db.Orders
+                        where c.iddh == d.id
+                        select c;
+            return View(CT_SP.Single());
+        }
+        #endregion
+        #endregion
+
+    }
+}
