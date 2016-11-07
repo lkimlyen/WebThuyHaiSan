5a4aef9a6eeedd4a1a6ddb22fd939f354aa2da16
khoi tran <khoitran@misfit.com>
2016-11-07 17:57:20 +0700

    Xong mấy task a nguyên giao

--- webanhnguyen/Controllers/BaseController.cs
+++ webanhnguyen/Controllers/BaseController.cs
@@ -10,5 +10,14 @@ namespace webanhnguyen.Controllers
     public abstract class BaseController : Controller
     {
         public databaseDataContext db = new databaseDataContext();
+    
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // Your logic here...
+            tbl_header hea = db.tbl_headers.SingleOrDefault(n => n.id == 1);
+            ViewBag.title = hea.tittle;
+            ViewBag.icon = hea.shortcuticon;
+            base.OnActionExecuting(filterContext);
+        }
     }
 }
